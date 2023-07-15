e3e0179a73617224f3ced974dfde14628e7ba158
Sabbir-Hossain12 <h.sabbir36@yahoo.com>
2023-07-16 00:48:39 +0600

    added login,sign up page

--- include/header.php
+++ include/header.php
@@ -8,7 +8,7 @@
         </a>
 
         <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-          <li><a href="#" class="nav-link px-2 text-white">Home</a></li>
+          <li><a href="inde.php" class="nav-link px-2 text-white">Home</a></li>
           <li><a href="#" class="nav-link px-2 text-white">Features</a></li>
           <li><a href="#" class="nav-link px-2 text-white">Pricing</a></li>
           <li><a href="#" class="nav-link px-2 text-white">FAQs</a></li>
@@ -28,8 +28,8 @@
         </form>
 
         <div class="text-end">
-          <button type="button" class="btn btn-outline-light me-2">Login</button>
-          <button type="button" class="btn btn-warning">Sign-up</button>
+          <button type="button"  class="btn btn-outline-light me-2"><a class="nav-link px-2 " href="login.php">Login</a></button>
+          <button type="button" class="btn btn-warning"><a class="nav-link px-2 " href="signup.php">Sign-up</a></button>
         </div>
       </div>
     </div>
